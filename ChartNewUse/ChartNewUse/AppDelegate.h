6bcad7337bfafb0f7362ee9c74646f9386fa79df
周灿华 <zhoucanhua@zhoucanhuadeMacBook-Air.local>
2017-10-17 18:10:31 +0800

    初始化

---
+++ ChartNewUse/ChartNewUse/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ChartNewUse
+//
+//  Created by 周灿华 on 2017/10/13.
+//  Copyright © 2017年 周灿华. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ChartNewUse/ChartNewUse/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ChartNewUse
+//
+//  Created by 周灿华 on 2017/10/13.
+//  Copyright © 2017年 周灿华. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
